c6094ae2a733109d00461cb598c5eb7b8721467c
repl.it user <replituser@example.com>
2020-11-06 10:39:58 +0000

    Implemented test with custom error messages

--- MainTest.java
+++ MainTest.java
@@ -1,8 +1,23 @@
 import org.junit.Test;
 import static org.junit.Assert.*; 
 
-class MainTest {
-  public static void main(String[] args) {
-    System.out.println("Hello Hahahahaha!");
+public class MainTest {
+  @Test
+  public void testNothing() {
+  }
+
+  @Test
+  public void testSomeStuff(){
+    try {
+      assertEquals("You are bad at math", 5, 2+3);
+
+      String s = " ";
+      assertNotNull("Your String is null", null);
+
+      assertTrue("Still bad at math", (3*7) > (4*5));
+
+    } catch(AssertionError e) {
+      System.out.println("\nError: " + e.getMessage());
+    }
   }
 }
